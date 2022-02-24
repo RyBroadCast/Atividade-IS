75acbed21486ce263a2c15034cdba2b369a2d84e
Lucas Broad <83410861+RyBroadCast@users.noreply.github.com>
2022-02-24 02:22:39 -0300

    Add files via upload

---
+++ zumbi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+
+int main(){
+
+	pit_t p;
+	p=fork();
+
+	if (p==0) {
+	printf("Filho, PID em %d",getpid());
+	printf("\nPai, PID em %d",getppid());
+	}
+
+	else {
+	printf("Pai, PID em %d",getpid());
+	printf("\nFilho, PID em %d",p);
+	}
+
+	return 0;
+}
